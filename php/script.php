365f220b8e1d4069e4819d71c5aa11f8aa62b2bb
Vincent Laisney <vincent.laisney@biblico.it>
2025-11-08 16:10:25 +0100

    New version and location of lualatex

--- php/script.php
+++ php/script.php
@@ -1,7 +1,7 @@
 <?php
     $PYTHON = 'export PATH="/usr/bin/python3:$PATH"; ';
     shell_exec($PYTHON."python3 ../python/ordomatic_main.py ${_POST["year"]}");
-    $LATEX = 'export PATH="/usr/bin/lualatex:$PATH"; ';
-    $retour = shell_exec("$LATEX lualatex ../latex/${_POST["year"]}.tex; mv ${_POST["year"]}.* ../latex; rm ../latex/*.log; rm ../latex/*.aux");
+    putenv('TEXMFVAR=' . __DIR__ . '/.texlive2024');
+    $retour = shell_exec("/usr/local/bin/lualatex ../latex/${_POST["year"]}.tex; mv ${_POST["year"]}.* ../latex; rm ../latex/*.log; rm ../latex/*.aux");
     print($retour);
 ?>
